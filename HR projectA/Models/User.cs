0887b9c950cf8b199b668ce953c99eba23d85102
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-04 17:17:05 +0400

    Write User Properties

--- HR projectA/Models/User.cs
+++ HR projectA/Models/User.cs
@@ -3,4 +3,11 @@ namespace ProjectX.Models;
 public class User
 {
     // user properties
+    public int UserId { get; set; }
+    public string Fullname { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+
+
 }
