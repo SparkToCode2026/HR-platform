64b0c9b51f65a0e62e454b2cd54296affb7f41dd
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-04 14:58:11 +0400

    removed unwanted lines from Programs.cs

--- HR projectA/Program.cs
+++ HR projectA/Program.cs
@@ -8,16 +8,14 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
-
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+       
         }
 
         app.UseHttpsRedirection();
@@ -29,19 +27,8 @@ public class Program
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+        
+          
 
         app.Run();
     }
