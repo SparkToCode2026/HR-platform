4d99fa0385f9794cfc8715964e21bdfc35b888e3
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-04 18:47:48 +0400

    Write company Properties

--- HR projectA/Models/User.cs
+++ HR projectA/Models/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     // user properties
     public int UserId { get; set; }
-    public string Fullname { get; set; }
+    public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string Role { get; set; }
