a1e3d155a97354c82d84181d07bfabcba95cba6b
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-04 19:48:47 +0400

    JopCategory model is added

---
+++ HR projectA/Models/JopCategory.cs
@@ -0,0 +1,11 @@
+namespace ProjectX.Models;
+
+public class JopCategory
+{
+    public int JopCategoryid { get; set; }
+    public String JopCategoryName { get; set; }
+    public string JopCategoryDesc { get; set; }
+    
+    
+  //  public List<"Jopposting"> JopPosting {get; set; } // navigation property
+}
