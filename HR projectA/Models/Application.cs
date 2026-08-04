4b6809391b253d0dd6634d7597fd6fcdd6fd8e6c
almajedalhinnai-max <almajedalhinnai@gmail.com>
2026-08-04 23:13:14 +0400

    Add Application model with properties

---
+++ HR projectA/Models/Application.cs
@@ -0,0 +1,14 @@
+namespace HRP.Models;
+
+public class Application
+{
+    public int ApplicationID { get; set; }
+    public DateTime AppliedAt { get; set; }
+    public string ApplicationStatus { get; set; } = string.Empty;
+
+    public int JobPostingID { get; set; }
+
+    public JobPosting? JobPosting { get; set; }
+    public Interview? Interview { get; set; }
+    public Offer? Offer { get; set; }
+}
