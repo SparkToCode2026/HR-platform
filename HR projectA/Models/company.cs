11b0ca0bdd012a5366ac4e81595a79acc9c656f3
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-04 23:22:24 +0400

    add nevegation properaty

---
+++ HR projectA/Models/company.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectX.Models
+{
+    public class company
+    {
+        public int CompanyId { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string CompanyDescription { get; set; }
+
+        public string Industry { get; set; }
+
+        public string CompanyWebsite { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string LocationStreet { get; set; }
+
+    }
+}
