016521f9a75cfa6279869d753abdcf60b1176dc9
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-05 16:35:16 +0400

    Add user navigation propertie

--- HR projectA/Models/User.cs
+++ HR projectA/Models/User.cs
@@ -9,8 +9,17 @@ public class User
     public string Password { get; set; }
     public string Role { get; set; }
 
+    //user navigation properties
+
+    public Resume Resume { get; set; }
 
     public List<UserSkill> UserSkills { get; set; }
 
+    public List<Application> Applications { get; set; }
+
+    public List<Notification> Notifications { get; set; }
+
+    public List<Review> Reviews { get; set; }
+ 
 
 }
