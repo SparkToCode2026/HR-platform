1a19af4545d3619c8cee897b1b0cb417fdc78103
sama saif <samaalramadhani@gmail.com>
2026-08-06 02:34:20 +0400

    added POST endpoint in ReviewController.cs

--- HR projectA/Controllers/ReviewController.cs
+++ HR projectA/Controllers/ReviewController.cs
@@ -1,6 +1,44 @@
-namespace ProjectX.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectX.Models;
 
-public class ReviewController
+namespace ProjectX.Controllers
 {
-    
+    [ApiController]
+    [Route("Review")]
+    public class ReviewController : ControllerBase
+    {
+        private ProjectContext context;
+
+        public ReviewController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        // Case 1 (POST): Submit a company review or interviewer rating.
+        [HttpPost("AddReview")]
+        public IActionResult AddReview(Review r)
+        {
+            context.Reviews.Add(r);
+
+            context.SaveChanges();
+
+            return Ok(r.ReviewId);
+        }
+        
+        // Case 2 (PUT/PATCH): Edit existing review text or rating score.
+        
+        
+        // Case 3 (PUT/PATCH): Admin approval status update (e.g., mark as "Approved" or "Flagged").
+        
+        // Case 4 (DELETE): Delete an inappropriate or obsolete review.
+        
+        // Case 5 (GET - List): Fetch reviews including related Company and reviewer User details.
+        
+        // Case 6 (GET - Find): Get review by ReviewId.
+        
+        // Case 7 (GET - Filter): Filter reviews by rating score (e.g., 4+ stars).
+        
+        // Case 8 (GET - Sort/Aggregate): Calculate average rating score (Average) per company.
+    }
 }
