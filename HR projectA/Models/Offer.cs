1730b23c75c2e56617aa8e815515b60575e8b28f
saja majid <saja27.m@gmail.com>
2026-08-05 13:28:16 +0400

    Add Application foreign key to Offer model

--- HR projectA/Models/Offer.cs
+++ HR projectA/Models/Offer.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ProjectX.Models
+namespace HRP.Models;
 {
-    internal class Offer
+    public class Offer
     {
-    }
+    public int OfferID { get; set; }
+    public decimal ProposalSalary { get; set; }
+    public string OfferState { get; set; } = string.Empty;
+    public string JobTitle { get; set; } = string.Empty;
+
+    //Foreign key
+    public int ApplicationID { get; set; } = string.Empty;
+
+
+
 }
