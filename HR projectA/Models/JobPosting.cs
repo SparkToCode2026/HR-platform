a14b5d245de6536b59dd454543cdc9440057f2d0
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-05 17:57:15 +0400

    added some edits on Apllication and jopposting

--- HR projectA/Models/JobPosting.cs
+++ HR projectA/Models/JobPosting.cs
@@ -10,14 +10,12 @@ public class JobPosting
     public string AcademicDegree { get; set; } = string.Empty;
     public int PositionsAvailable { get; set; }
     public DateTime JobDeadline { get; set; }
-    [ForeignKey("Department")]
-    public int DepartmentID { get; set; }
+  
     [ForeignKey("JobCategory")]
     public int JobCategoryID { get; set; }
     [ForeignKey("Company")]
     public int CompanyID { get; set; }
-
-    public Department? Department { get; set; }
+    
     public JopCategory? JobCategory { get; set; }
     public company? Company { get; set; }
     public List<Application> Applications { get; set; } = new();
