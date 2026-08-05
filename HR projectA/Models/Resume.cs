94f14e52da82bfdf06f3bdefee5601c39dc9970c
Ibtihaj <36j207@utas.edu.om>
2026-08-05 09:41:21 +0400

    Resume model is added

---
+++ HR projectA/Models/Resume.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectX.Models
+{
+    public class Resume
+    {
+        public int Resume_id { get; set; }
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Education { get; set; }
+
+    }
+}
