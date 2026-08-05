04582feaa64cd3261716bcb5435c1bcb39e073c1
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-05 23:58:42 +0400

    Company Registration

---
+++ HR projectA/Controllers/CompanyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectX.Models;
+
+namespace ProjectX.Controllers
+{
+
+    [ApiController]
+    [Route("Company")]
+    public class CompanyController
+    {
+        private readonly ProjectContext context;
+
+        public CompanyController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        // POST: Register Company
+        [HttpPost("Register")]
+        public IActionResult Register(company c)
+        {
+            context.Companies.Add(c);
+            context.SaveChanges();
+            return Ok(c.CompanyId);
+        }
+    }
+}
