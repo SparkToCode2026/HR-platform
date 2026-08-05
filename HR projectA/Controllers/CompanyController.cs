f0fa532710cdf8a6cec9b378de1affd88f9fafcf
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-06 00:01:27 +0400

    Ubdate company

--- HR projectA/Controllers/CompanyController.cs
+++ HR projectA/Controllers/CompanyController.cs
@@ -23,5 +23,25 @@ namespace ProjectX.Controllers
             context.SaveChanges();
             return Ok(c.CompanyId);
         }
+
+        // PUT: Update Company 
+        [HttpPut("UpdateCompany")]
+        public IActionResult UpdateCompany(int id, company newCompany)
+        {
+            var c = context.Companies.FirstOrDefault(x => x.CompanyId == id);
+            if (c == null) return NotFound();
+
+            c.CompanyName = newCompany.CompanyName;
+            c.CompanyDescription = newCompany.CompanyDescription;
+            c.CompanyWebsite = newCompany.CompanyWebsite;
+            c.Phone = newCompany.Phone;
+            c.Email = newCompany.Email;
+            c.LocationStreet = newCompany.LocationStreet;
+            context.SaveChanges();
+            return Ok("Company updated");
+        }
+
+        //
+
     }
 }
