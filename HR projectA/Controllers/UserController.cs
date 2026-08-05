576c63a8f0a412be51cafc46830b01d3effe96b5
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-08-05 21:41:24 +0400

    Creat new User

---
+++ HR projectA/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectX.Models;
+
+namespace ProjectX.Controllers
+{
+    public class UserController
+    {
+        ProjectContext context = new ProjectContext();
+        public void AddUser()
+        {
+            // Logic to create a new user
+            User U=new User();
+            Console.WriteLine("Enter your username:");
+            U.Username = Console.ReadLine();
+            Console.WriteLine("Enter User ID:");
+            U.UserId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your email:");
+            U.Email = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            U.Password = Console.ReadLine();
+            Console.WriteLine("Enter your role (Admin/Candidate/Employer):");
+            U.Role = Console.ReadLine();
+
+            context.Users.Add(U);
+            context.SaveChanges();
+
+        }
+
+
+    }
+}
