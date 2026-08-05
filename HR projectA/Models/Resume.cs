e31a6ff37e962018d56072d0efd8df0ffe853f80
Ibtihaj <36j207@utas.edu.om>
2026-08-05 09:49:28 +0400

    Skill model is added

--- HR projectA/Models/Resume.cs
+++ HR projectA/Models/Resume.cs
@@ -4,9 +4,7 @@
     {
         public int Resume_id { get; set; }
         public string Title { get; set; }
-
         public string Summary { get; set; }
-
         public string Education { get; set; }
 
     }
---
+++ HR projectA/Models/Skill.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectX.Models
+{
+    public class Skill
+    {
+        public int Skill_id { get; set; }
+        public string Skill_Name { get; set; }
+        public string Skill_Category { get; set; }
+
+    }
+}
