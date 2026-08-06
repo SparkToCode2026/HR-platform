051e5bf6fc465ae9ba5b53425b2d239732871e2f
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-06 10:31:25 +0400

    Get Department By id Add Department Update Department Name Update Department Description Delete Department Get Comapny Detail Through Department

---
+++ HR projectA/Controllers/DepartmentController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectX.Models;
+
+namespace ProjectX.Controllers;
+[ApiController]
+[Route("DepartmentController")]
+public class DepartmentController:ControllerBase
+{
+    private ProjectContext Context;
+
+    public DepartmentController(ProjectContext _Context)
+    {
+        Context = _Context;
+    }
+
+    [HttpPost("Add Department")]
+    public void ADD_Department(Department D)
+    {
+        Context.Departments.Add(D);
+        Context.SaveChanges();
+    }
+    [HttpPatch("Update Department")]
+    public void Update_Department(int Id, String Name)
+    {
+        Department Updated_depart = Context.Departments.FirstOrDefault(D => D.DepartmentID == Id);
+        Updated_depart.DepartmentName = Name;
+        Context.Departments.Update(Updated_depart);
+        Context.SaveChanges();
+    }
+    [HttpPatch("Update Department Description")]
+    public void Update_department_desc(int id, String Describtion)
+    {
+
+        Department Updated_depart = Context.Departments.FirstOrDefault(D => D.DepartmentID == id);
+        Updated_depart.DepartmentDesc = Describtion;
+        Context.Departments.Update(Updated_depart);
+        Context.SaveChanges();
+    }
+    [HttpDelete("Delete Department")]
+    public void Remove_Department(int Id)
+    {
+        Department Removed_Department = Context.Departments.FirstOrDefault(C => C.DepartmentID == Id);
+        Context.Departments.Remove(Removed_Department);
+        Context.SaveChanges();
+
+    } // enter the Department id , to see the comapny detalis that belong to
+    [HttpGet("Get company detail")]
+    public company Return_Company(int id)
+    {
+        var Company = Context.Departments
+            .Where(d => d.DepartmentID == id)
+            .Select(d => d.CompanyA) // Projects directly to the Company object
+            .FirstOrDefault();
+
+        return Company;
+    }
+    
+    
+    
+}
