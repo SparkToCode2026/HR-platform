ee7ff2cfd63f20d819cea35e12a2d6b27e6931c8
saja majid <saja27.m@gmail.com>
2026-08-07 01:17:44 +0400

    Add case 1

--- HR projectA/Controllers/OffersController.cs
+++ HR projectA/Controllers/OffersController.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using HRP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace ProjectX.Controllers
+namespace ProjectX.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OffersController : ControllerBase
 {
-    internal class OffersController
+    private readonly ProjectContext _context;
+
+    public OffersController(ProjectContext context)
     {
+        _context = context;
     }
-}
+
+    // Case 1: Create a new offer
+    [HttpPost]
+    public async Task<IActionResult> CreateOffer(CreateOfferRequest request)
+    {
+        var applicationExists = await _context.Applications
+            .AnyAsync(a => a.ApplicationID == request.ApplicationID);
+
+        if (!applicationExists)
+        {
+            return BadRequest("The application does not exist.");
+        }
+
+        var offerAlreadyExists = await _context.Offers
+            .AnyAsync(o => o.ApplicationID == request.ApplicationID);
+
+        if (offerAlreadyExists)
+        {
+            return BadRequest("An offer already exists for this application.");
+        }
+
+        if (request.ProposalSalary <= 0)
+        {
+            return BadRequest("The proposed salary must be greater than zero.");
+        }
+
+        var offer = new Offer
+        {
+            ProposalSalary = request.ProposalSalary,
+            OfferState = request.OfferState,
+            JobTitle = request.JobTitle,
+            ApplicationID = request.ApplicationID
+        };
+
+        _context.Offers.Add(offer);
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetOfferById),
+            new { id = offer.OfferID },
+            offer);
+    }
+
