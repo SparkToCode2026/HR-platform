d04ed526602b4f27fab8991a05807eeff63330e7
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-08 15:45:19 +0400

    Added the [jasonignore]

--- HR projectA/Models/Interview.cs
+++ HR projectA/Models/Interview.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace HRP.Models;
 
 public class Interview
 {
+    [Key]
+    [JsonIgnore]
     public int InterviewID { get; set; }
 
     public DateTime InterviewDate { get; set; }
@@ -20,5 +24,6 @@ public class Interview
     public int ApplicationID { get; set; }
 
     // Navigation Property
+    [JsonIgnore]
     public Application Application { get; set; } = null!;
 }
