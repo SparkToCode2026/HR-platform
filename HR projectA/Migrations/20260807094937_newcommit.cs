e09904d50928464d0ed1f8d084ceac98fe5f0d0f
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-09 22:39:45 +0400

    add user migration file

---
+++ HR projectA/Migrations/20260807094937_newcommit.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectX.Migrations
+{
+    /// <inheritdoc />
+    public partial class newcommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "users",
+                newName: "PhoneNumber");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Companies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Companies");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "users",
+                newName: "Password");
+        }
+    }
+}
