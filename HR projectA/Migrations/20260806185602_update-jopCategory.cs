af6539051dbbb760d92e7b49d18163e8968057c5
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-09 22:36:00 +0400

    .

---
+++ HR projectA/Migrations/20260806185602_update-jopCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectX.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatejopCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CategorysTATUS",
+                table: "JopCategories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategorysTATUS",
+                table: "JopCategories");
+        }
+    }
+}
