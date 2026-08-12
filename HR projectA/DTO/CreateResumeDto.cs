2b67237806bc35802f7fe5e3e5f3e9f64b1a5432
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-12 15:44:57 +0400

    ADDED new Dto for resume

---
+++ HR projectA/DTO/CreateResumeDto.cs
@@ -0,0 +1,8 @@
+namespace ProjectX.DTOs;
+
+public class CreateResumeDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty;
+    public string Education { get; set; } = string.Empty;
+}
