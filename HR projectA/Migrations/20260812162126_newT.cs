438063436d62b772c75e67ea357cf493872d50db
saif alharrasi <saifalharrasi57@gmail.com>
2026-08-14 20:28:15 +0400

    migration files

---
+++ HR projectA/Migrations/20260812162126_newT.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectX.Migrations
+{
+    /// <inheritdoc />
+    public partial class newT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_CompanyId",
+                table: "users",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_Companies_CompanyId",
+                table: "users",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_Companies_CompanyId",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_CompanyId",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "users");
+        }
+    }
+}
